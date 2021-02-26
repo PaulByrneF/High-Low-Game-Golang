12808031cd267ba201b90ffa8ea39f961897969f
Paul Byrne <paul.byrne.f@gmail.com>
2021-02-26 11:19:15 +0000

    fixed bug of card not being assigned to the nextCard & added readme description

---
+++ README.md
@@ -0,0 +1,11 @@
+Game of High or Low: Go (GoLang)
+------------------------------------------------
+
+This game was created to practice my go skills.
+
+1. The game generates a full deck of cards, which the dealer shuffles when the game starts.
+2. The shuffled deck is returned and a card is displayed to the user.
+3. The user must specify is the next card will be higher or lower
+4. The game will determine the success of the users choice and will call the implemented dealCard function on the deck type.
+5. If the user is successful, the game will continue to the next round and will keep running until the deck has 2 cards left.
+6. If the user is unsuccessful at any round, the dealer will stop the game
--- main.go
+++ main.go
@@ -4,36 +4,61 @@ import "fmt"
 
 func main() {
 
+	//Intialise, delcare & assign variable deck to the deck returned from function
 	deck := generateDeck()
+
+	//Initiliase & declare card, nextCard of type card
+	//Initialise & declare guess type string
 	var card, nextCard card
 	var guess string
 
+	//Print welcome message
 	fmt.Println("Welcome to a game of High or Low\nPlease wait while the dealer shuffles the cards ... \n")
+
+	//use deck as receiver which implements shuffle function to randomly swap position of cards in deck
 	deck.shuffle()
 
+	//Show message indicating the first round
 	fmt.Println("\nRound: ", 1)
+
+	//Call the implented function "dealDeck()" that returns the first card and updated deck
 	card, deck = deck.dealCard()
 	printCard(card)
 
+	//Iterate over the cards in the deck slice
 	for i := 1; i < len(deck)-1; i++ {
 
+		//Call the implented function "dealDeck()" that returns the first card and updated deck
 		nextCard, deck = deck.dealCard()
+
+		//use scan function to capture input from user and assign the pointer to the in memory address of value
 		fmt.Scanf("%v", &guess)
 
+		//If clause to check if user chose higher
 		if guess == "higher" {
+
+			//nested if clause to check if card.value is more than the presented card.value
 			if nextCard.value > card.value {
 				fmt.Println("Well done...")
 				printCard(nextCard)
-
+				continue
+				//else to capture wrong answer from end user
 			} else {
 				fmt.Println("Unlucky...")
 				printCard(nextCard)
+
+				//The user loses, break loop
 				break
 			}
+			//else clause to capture the value of lower from end user
 		} else {
+
+			//check if the next card value is lower than the target card
 			if nextCard.value < card.value {
 				fmt.Println("Well done...")
 				printCard(nextCard)
+				continue
+				//Else statement to capture the wrong answer and break loop
 			} else {
 				fmt.Println("Unlucky...")
 				printCard(nextCard)
@@ -41,13 +66,17 @@ func main() {
 			}
 		}
 
-		nextCard = card
+		//if the user is correct, assign the card to nextCard
+		card = nextCard
+
+		//Keep running loop until there is two cards left in the deck
 		if len(deck) == 2 {
 			break
 		}
 	}
 }
 
+//Print the card title
 func printCard(c card) {
 	fmt.Println("Your card is: ", c.card)
 }
